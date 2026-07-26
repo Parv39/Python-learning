b403b5de9657bd477bdd65edc998f4fbf1bea1fc
Parv39 <parvsharma550@gmail.com>
2026-07-26 21:53:45 +0530

    Add files via upload

---
+++ Chapter-1/2program.py
@@ -0,0 +1,13 @@
+#Hello single line comment used for only single line
+
+"""
+Hey this is multiline comment
+"""
+
+print("a")
+
+'''
+This is also multiline comment
+Not technically
+Multiline string (often used like a comment)
+'''
---
+++ Chapter-1/practice1.py
@@ -0,0 +1,11 @@
+print("""
+Hey
+bye
+lol
+      it's
+      "nice"
+""")
+
+print('''
+this also works it's "better"
+      ''')
