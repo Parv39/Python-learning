6b41b9b5ce95d33d6ea7947f0af6a79d8177858e
Parv39 <parvsharma550@gmail.com>
2026-08-15 21:14:33 +0530

    Delete Chapter 6 directory

--- Chapter 6/1Program.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter Age:"))
-
-if(a>=18):
-    print("Damn!")
-
-else:
-    print("Shit!")
--- Chapter 6/3program.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter Age:"))
-
-if (a>=18):
-    print("Valid!")
-else:
-    print("Invalid")
