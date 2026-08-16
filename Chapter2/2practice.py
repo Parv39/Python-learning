7600c82d9b9ec3bbc335998b5b5482f1390f3b03
Parv39 <parvsharma550@gmail.com>
2026-08-16 16:56:34 +0530

    Add files via upload

---
+++ Chapter2/2practice.py
@@ -0,0 +1,9 @@
+a = 50
+b = 10
+print("Remainder of numbers", a%b)
+
+#Better version
+c = float(input("Enter the number:"))
+d = float(input("Enter the number:"))
+
+print("Remainder of numbers:", c%d)
