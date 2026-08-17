2317bc6e4ab77e0d03c3e1ba2e28c90878b7367e
Parv39 <parvsharma550@gmail.com>
2026-08-17 21:36:17 +0530

    Add files via upload

---
+++ Chapter2/3program.py
@@ -0,0 +1,20 @@
+a=5
+b=10
+c=a+b
+print(c)
+#This is arithmatic operator
+a=4-2 #Assignment operator 
+b=10
+b-=5
+b+=11
+print(b)
+#Comparison operators value is always boolean
+d = 10>5
+e = 10>=9
+f = 10<=10
+g = 5!=5
+print(d)
+print(g)
+#Logical operators
+x = True or False
+print(x)
