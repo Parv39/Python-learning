ad867635949ae4c8b69b51e091dde08070bb726e
Parv39 <parvsharma550@gmail.com>
2026-08-21 22:35:02 +0530

    Add files via upload

---
+++ Chapter3/3program.py
@@ -0,0 +1,9 @@
+a = "parv meets jesus"
+print(len(a))
+print(a.endswith("v"))
+print(a.startswith("p")) #This function is case sensitive A and a is different
+print(a.capitalize()) #Only capatalize 1st char
+print(a.title())
+print(a.lower())
+print(a.upper())
+print(a.replace("parv","Hello"))
