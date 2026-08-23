764154608ee5afb625130e441d78c7567626e8a3
Parv39 <parvsharma550@gmail.com>
2026-08-23 22:26:43 +0530

    Add files via upload

---
+++ Chapter4/1program.py
@@ -0,0 +1,7 @@
+#Container to store different datatypes in py is list and are muttable
+#list also start from index 0
+a = ["Apple","Hello",10.5,5,"Hmm"]
+print(a[3])
+a[1] = "hola!"
+print(a)
+print(a[1:4])
