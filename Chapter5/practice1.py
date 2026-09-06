4c7ce9cda73525ea9d6ffe2d1fe1662bbfffa1db
Parv39 <parvsharma550@gmail.com>
2026-09-06 23:43:06 +0530

    Add files via upload

---
+++ Chapter5/practice1.py
@@ -0,0 +1,9 @@
+a= {
+    "madad":"Help",
+    "bili":"cat",
+    "bhago":"run"
+}
+word=input("Enter the word you want meaning of:")
+print(a[word])
+#This means:
+# “Go to dictionary a and fetch the value whose key is equal to word.”
