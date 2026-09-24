abcf985e44bee192abd4c635598bec7c9af81fd8
Parv39 <parvsharma550@gmail.com>
2026-09-24 18:44:02 +0530

    Add files via upload

---
+++ Chapter7/3program.py
@@ -0,0 +1,17 @@
+for i in range(4): #From 0:4
+    print(i)
+
+a = [1,2,3,4]
+for item in a:
+    print(item)
+else:
+    print("Ho gya oe")
+
+b = "Parv"
+for item in b:
+    print(b) #Prints parv 5 times
+
+x = "Parv"
+for item in x:
+    print(item) #Prints single single char 
+#We are iterating in loops
