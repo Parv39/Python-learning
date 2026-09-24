fb498c188657fcb789d9aff2f164d318a5201c06
Parv39 <parvsharma550@gmail.com>
2026-09-24 18:44:17 +0530

    Add files via upload

---
+++ Chapter7/4program.py
@@ -0,0 +1,12 @@
+for a in range(0,50):
+    print(a)
+    if a == 10: #Exit the loop
+        break
+
+#Used to come out of loop 
+
+for a in range(0,13):
+    if a == 10: #Skip this iteration it is i=1,i=2
+        continue
+    print(a) #This is the right syntax if print is above does not continue
+
